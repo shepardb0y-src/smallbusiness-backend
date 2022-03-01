71774988a4f4af7e77f1d1b1e894a20a548919f0
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-03-01 14:14:09 -0500

    created end point /allusers and exception package

--- src/main/java/com/fabiobenoit/backend/controllers/UsersControllers.java
+++ src/main/java/com/fabiobenoit/backend/controllers/UsersControllers.java
@@ -3,13 +3,16 @@ package com.fabiobenoit.backend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fabiobenoit.backend.exceptions.ResourceNotFoundException;
 import com.fabiobenoit.backend.models.User;
 import com.fabiobenoit.backend.repositories.UserRepository;
 
@@ -22,12 +25,16 @@ public class UsersControllers {
 	private UserRepository userRepo;
 	
 	@GetMapping("allusers")
-	public List<User> getAllStudents() {
-//		You can pass on some sorting logic inside findAll() 
+	public List<User> getAllUsers() {
+
 		return userRepo.findAll();
 	}
-	@PostMapping("addstudent")
-	public User newStudent(@RequestBody User user) {
-		return userRepo.save(user);
-	}
+
+
+	@GetMapping("user/{id}")
+	public ResponseEntity<User> getStudentById(@PathVariable int id) {
+		User user = userRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Student not found."));
+				return ResponseEntity.ok(user);
+}
 }
