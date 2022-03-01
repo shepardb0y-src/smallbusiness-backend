981112fc10064429086c83330f8ac2154e3073ae
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-03-01 11:38:30 -0500

    application context done

---
+++ src/main/java/com/fabiobenoit/backend/SmallbusinessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fabiobenoit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallbusinessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallbusinessBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabiobenoit/backend/SmallbusinessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiobenoit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallbusinessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
