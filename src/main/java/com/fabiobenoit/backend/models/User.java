cae4ad03b8c4e8406a65096abd5e75cae9d3900f
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-03-01 12:05:01 -0500

    set up user model

--- src/main/java/com/fabiobenoit/backend/models/User.java
+++ src/main/java/com/fabiobenoit/backend/models/User.java
@@ -1,5 +1,69 @@
 package com.fabiobenoit.backend.models;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column
+	private String firstname;
+	@Column
+	private String lastname;
+	@Column
+	private String streetname;
+	@Column
+	private String state;
+	@Column
+	private String zipcode;
+	@Column
+	private String comments;
+	public int getId() {
+		return id;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getStreetname() {
+		return streetname;
+	}
+	public void setStreetname(String streetname) {
+		this.streetname = streetname;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
 }
