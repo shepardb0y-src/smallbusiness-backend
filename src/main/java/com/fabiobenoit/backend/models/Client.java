085901c6ea30a204f3730c78ed1e09cc885f7f0c
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-03-03 11:12:56 -0500

    genterated getters and setter for client class

--- src/main/java/com/fabiobenoit/backend/models/Client.java
+++ src/main/java/com/fabiobenoit/backend/models/Client.java
@@ -1,5 +1,31 @@
 package com.fabiobenoit.backend.models;
 
-public class Client {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+public class Client {
+	@Entity
+	@Table(name="classes")
+	public class User {
+		@Id
+		@Column
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
+		private int id;
+		@Column
+		private String username;
+		public int getId() {
+			return id;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+}
 }
