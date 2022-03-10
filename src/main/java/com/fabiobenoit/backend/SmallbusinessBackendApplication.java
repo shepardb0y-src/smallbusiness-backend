a658734de232b6279629f20ff624aa9a50eaf9f0
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-03-09 19:12:08 -0500

    removed scanner

--- src/main/java/com/fabiobenoit/backend/SmallbusinessBackendApplication.java
+++ src/main/java/com/fabiobenoit/backend/SmallbusinessBackendApplication.java
@@ -2,10 +2,9 @@ package com.fabiobenoit.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.yawintutor.controller"})
+
 
 public class SmallbusinessBackendApplication {
 
