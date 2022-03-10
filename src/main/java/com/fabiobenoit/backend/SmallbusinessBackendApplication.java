7cd3adc62fe078d16fe3b475f46561f52ba66a8c
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2022-03-09 19:02:18 -0500

    added scanner

--- src/main/java/com/fabiobenoit/backend/SmallbusinessBackendApplication.java
+++ src/main/java/com/fabiobenoit/backend/SmallbusinessBackendApplication.java
@@ -2,8 +2,11 @@ package com.fabiobenoit.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.yawintutor.controller"})
+
 public class SmallbusinessBackendApplication {
 
 	public static void main(String[] args) {
